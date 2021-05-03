c4d87c5c7975d874bbebcdca95fe97c8f69bcb55
Dennis Müller <dennis25mueller@yahoo.de>
2021-05-03 22:08:39 +0200

    added missing code from last commit

--- Source/PrototypeGame/PrototypeGameGameModeBase.cpp
+++ Source/PrototypeGame/PrototypeGameGameModeBase.cpp
@@ -3,3 +3,15 @@
 
 #include "PrototypeGameGameModeBase.h"
 
+APrototypeGameGameModeBase::APrototypeGameGameModeBase() {
+
+}
+
+
+void APrototypeGameGameModeBase::BeginPlay() {
+	Super::BeginPlay();
+
+	//Spawn Player Client
+	GetWorld()->SpawnActor<AClient>(AClient::StaticClass(), DefaultSpawnPosition, FRotator(0.0f, 0.0f, 0.0f));
+	UE_LOG(LogTemp, Warning, TEXT("Client Spawned"));
+}
--- Source/PrototypeGame/PrototypeGameGameModeBase.h
+++ Source/PrototypeGame/PrototypeGameGameModeBase.h
@@ -4,14 +4,25 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+
+#include "Client.h"
+
 #include "PrototypeGameGameModeBase.generated.h"
 
+#define DefaultSpawnPosition FVector(0.0f, 0.0f, 20.0f)
+
 /**
- * 
+ *
  */
 UCLASS()
 class PROTOTYPEGAME_API APrototypeGameGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+		APrototypeGameGameModeBase();
+
+	void BeginPlay();
+
+private:
+
 };
